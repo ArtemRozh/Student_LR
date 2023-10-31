24120d168e767b79e07fa40c593e027d0fd2616e
ArtemRozh <artemrozh2@gmail.com>
2023-10-31 17:51:26 +0200

    Added new .cpp file

---
+++ лр7.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <Math.h>
+#include <Windows.H>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int y, k;
+    float res,  mult = 1;
+    cout << "Введіть першу змінну k\nk = ";
+    cin >> k;
+    cout << "Введіть другу змінну y\ny = ";
+    cin >> y;
+    for (int i = 1; i < 11; i++) {
+        mult = mult * log(k*y);
+    }
+    res = y + mult + 2;
+    cout << "Результат рівняння = " << res;
+}
